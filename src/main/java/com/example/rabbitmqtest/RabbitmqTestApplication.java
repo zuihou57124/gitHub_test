8e074d9120a14fda51bd721b3055da91ccf1d324
zuihou57124 <1545409483@qq.com>
2020-04-09 16:53:37 +0800

    test

--- src/main/java/com/example/rabbitmqtest/RabbitmqTestApplication.java
+++ src/main/java/com/example/rabbitmqtest/RabbitmqTestApplication.java
@@ -3,9 +3,11 @@ package com.example.rabbitmqtest;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableRabbit
+@EnableCaching
 public class RabbitmqTestApplication {
 
     public static void main(String[] args) {
